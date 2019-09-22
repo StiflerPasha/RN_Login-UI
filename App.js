82588f742081fb6f438f9c3fc40a31552d94ac93
StiflerPasha <dvoynospavel@gmail.com>
2019-09-22 20:36:11 +0300

    init commit

--- App.js
+++ App.js
@@ -1,43 +1,11 @@
-import React                                              from 'react';
-import { StyleSheet, Text, View, ImageBackground, Image } from 'react-native';
+import React from 'react';
+import Login from './screens/Login';
 
-import bgImage from './images/bg.png';
-import logo    from './images/logo.png';
 
 const App = () => {
    return (
-	  <ImageBackground source={ bgImage } style={ styles.backgroundContainer }>
-		 <View style={ styles.logoContainer }>
-			<Image source={ logo } style={ styles.logo } />
-			<Text style={ styles.logoText }>REACT NATIVE</Text>
-		 </View>
-	  </ImageBackground>
+	  <Login />
    );
 };
-
-const styles = StyleSheet.create({
-   backgroundContainer: {
-	  flex: 1,
-	  width: null,
-	  height: null,
-	  alignItems: 'center',
-	  justifyContent: 'center',
-   },
-   logoContainer: {
-	  alignItems: 'center',
-   },
-   logo: {
-	  width: 150,
-	  height: 150,
-	  opacity: .9,
-   },
-   logoText: {
-	  color: 'white',
-	  fontSize: 20,
-	  fontWeight: '500',
-	  marginTop: 10,
-	  opacity: .5,
-   },
-});
 
 export default App;
--- screens/Login.js
+++ screens/Login.js
@@ -1,23 +1,55 @@
-import React                           from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import React                                                         from 'react';
+import { View, StyleSheet, TextInput, Image, Text, ImageBackground } from 'react-native';
+
+import bgImage from '../images/bg.png';
+import logo    from '../images/logo.png';
 
 const Login = () => {
    return (
-	  <View style={ styles.inputContainer }>
-		 <View>
-			<TextInput />
+	  <ImageBackground source={ bgImage } style={ styles.backgroundContainer }>
+		 <View style={ styles.logoContainer }>
+			<Image source={ logo } style={ styles.logo } />
+			<Text style={ styles.logoText }>REACT NATIVE</Text>
 		 </View>
-		 <View>
-			<TextInput />
+		 <View style={ styles.inputContainer }>
+			<View>
+			   <TextInput />
+			</View>
+			<View>
+			   <TextInput />
+			</View>
+			<View>
+			   <TextInput />
+			</View>
 		 </View>
-		 <View>
-			<TextInput />
-		 </View>
-	  </View>
+	  </ImageBackground>
    );
 };
 
+
 const styles = StyleSheet.create({
+   backgroundContainer: {
+	  flex: 1,
+	  width: null,
+	  height: null,
+	  alignItems: 'center',
+	  justifyContent: 'center',
+   },
+   logoContainer: {
+	  alignItems: 'center',
+   },
+   logo: {
+	  width: 150,
+	  height: 150,
+	  opacity: .9,
+   },
+   logoText: {
+	  color: 'white',
+	  fontSize: 20,
+	  fontWeight: '500',
+	  marginTop: 10,
+	  opacity: .5,
+   },
    inputContainer: {},
 });
 
